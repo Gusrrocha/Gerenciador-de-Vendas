26f9a5eebae8be4036f8980c816af3e7db3a7387
Gusrrocha <95317020+Gusrrocha@users.noreply.github.com>
2021-12-06 22:26:41 -0300

    Principal

--- controller/cliente_page.py
+++ controller/cliente_page.py
@@ -1,7 +1,19 @@
-
 from qt_core import *
+from controller.cad_cliente import CadClienteWindow
+
 FILE_UI = 'view/cliente_page.ui'
+
 class ClientesPage(QWidget):
     def __init__(self):
         super().__init__()
         uic.loadUi(FILE_UI, self)
+        
+        self.cliente_window = None
+    
+        # evento do botão nova peça
+        self.novo_cliente_btn.clicked.connect(self.novo_cliente)
+
+    def novo_cliente(self):
+        # criação da janela de cadastro
+        self.cliente_window = CadClienteWindow()
+        self.cliente_window.show()
