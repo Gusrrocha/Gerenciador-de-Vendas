d6d90371e0397220482f498e53f1a9974b793c97
Gusrrocha <95317020+Gusrrocha@users.noreply.github.com>
2021-12-08 21:01:19 -0300

    Painel Principal

--- model/venda.py
+++ model/venda.py
@@ -4,9 +4,10 @@ from model.cliente import Cliente
 
 
 class Venda():
-    def __init__(self, id, cliente, lista_de_itens):
+    def __init__(self, id, cliente, data, lista_de_itens):
         self.id = id
         self.cliente = cliente
+        self.data = data
         self.lista_de_itens = lista_de_itens
 
     def valorTotal(self):
