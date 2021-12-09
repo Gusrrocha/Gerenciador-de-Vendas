dfa994d2650d3f2a3f61d47392121890cffc2275
Gusrrocha <95317020+Gusrrocha@users.noreply.github.com>
2021-12-08 21:00:10 -0300

    Update main_window.py

--- controller/main_window.py
+++ controller/main_window.py
@@ -4,6 +4,7 @@ from qt_core import *
 from controller.cliente_page import ClientesPage
 from controller.pecas_page import PecasPage
 from controller.resumo_page import ResumoPage
+from controller.vendas_page import VendasPage
 FILE_UI = 'view/mainwindow.ui'
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -14,15 +15,18 @@ class MainWindow(QMainWindow):
         self.resumo_page = ResumoPage()
         self.clientes_page = ClientesPage()
         self.pecas_page = PecasPage()
+        self.vendas_page = VendasPage()
 
         # Adicionar as páginas ao painel_principal
         self.painel_principal.insertWidget(0,self.resumo_page)
         self.painel_principal.insertWidget(1,self.clientes_page)
         self.painel_principal.insertWidget(2,self.pecas_page)
+        self.painel_principal.insertWidget(3,self.vendas_page)
         # define o evento/ação dos botão:
         self.resumo_btn.clicked.connect(self.show_resumo)
         self.clientes_btn.clicked.connect(self.show_clientes)
         self.pecas_btn.clicked.connect(self.show_pecas)
+        self.vendas_btn.clicked.connect(self.show_vendas)
     def show_resumo(self):
         self.painel_principal.setCurrentIndex(0)
     
@@ -31,3 +35,6 @@ class MainWindow(QMainWindow):
 
     def show_pecas(self):
         self.painel_principal.setCurrentIndex(2)
+
+    def show_vendas(self):
+        self.painel_principal.setCurrentIndex(3)
